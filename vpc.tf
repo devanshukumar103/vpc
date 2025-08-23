9621a70c8b6b7a53be79d32ea33251c6c3696c45
Ashok Kumar <ashok.kumar2@virginmediao2.co.uk>
2025-08-23 22:55:09 +0530

    add

--- vpc.tf
+++ vpc.tf
@@ -1,17 +1,17 @@
-# resource "aws_vpc" "myvpc1" {
-#   cidr_block = "10.0.0.0/16"
-#   tags = {
-#     terraform = "true"
-#     Name = "myvpc"
-#   }
-# }
+resource "aws_vpc" "myvpc1" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    terraform = "true"
+    Name = "myvpc1"
+  }
+}
 
-# resource "aws_subnet" "publicsubnet" {
-#   vpc_id = aws_vpc.myvpc1.id
-#   cidr_block = "10.0.1.0/24"
-#   tags = {
-#     terraform = "true"
-#     Name = "publicsubnet"
-#   }
+resource "aws_subnet" "publicsubnet" {
+  vpc_id = aws_vpc.myvpc1.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    terraform = "true"
+    Name = "publicsubnet"
+  }
 
-# }
+}
