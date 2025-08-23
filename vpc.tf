e8be3c5d758ebbf353ecc4035d3c86a8ac453b6a
Devanshu <devanshukumar103@gmail.com>
2025-08-23 20:53:49 +0530

    deleting vpc

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,11 @@
 #VPC using terrform
-resource "aws_vpc" "myvpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    terraform = "true"
-    Name = "myvpc"
-  }
-}
+# resource "aws_vpc" "myvpc" {
+#   cidr_block = "10.0.0.0/16"
+#   tags = {
+#     terraform = "true"
+#     Name = "myvpc"
+#   }
+# }
 
 #Subnet using terraform
 # resource "aws_subnet" "publicsubnet" {
