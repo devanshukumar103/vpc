6acecaa1294b02fd9aa56970961da02f8c9adfe6
devanshukumar103 <96079207+devanshukumar103@users.noreply.github.com>
2025-08-24 21:03:23 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,13 +1,13 @@
-resource "aws_vpc" "myvpc1" {
+resource "aws_vpc" "myvpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
     terraform = "true"
-    Name = "myvpc1"
+    Name = "myvpc2"
   }
 }
 
 resource "aws_subnet" "publicsubnet" {
-  vpc_id = aws_vpc.myvpc1.id
+  vpc_id = aws_vpc.myvpc.id
   cidr_block = "10.0.1.0/24"
   tags = {
     terraform = "true"
