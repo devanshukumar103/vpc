1a984a98faa87186f825a6e762cf3a0aa778cb5c
Devanshu <devanshukumar103@gmail.com>
2025-08-24 21:23:51 +0530

    kaku

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "myvpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
     terraform = "true"
-    Name = "myvpc"
+    Name = "kaku"
   }
 }
 
@@ -11,7 +11,7 @@ resource "aws_subnet" "publicsubnet" {
   cidr_block = "10.0.1.0/24"
   tags = {
     terraform = "true"
-    Name = "publicsubnet"
+    Name = "kaku-subnet"
   }
 
 }
