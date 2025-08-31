be4f20032a383198e6608a283ade16590be5ae35
Devanshu <devanshukumar103@gmail.com>
2025-08-31 11:12:24 +0530

    deleting

--- vpc.tf
+++ vpc.tf
@@ -1,17 +1,17 @@
-resource "aws_vpc" "myvpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    terraform = "true"
-    Name = "testing"
-  }
-}
+# resource "aws_vpc" "myvpc" {
+#   cidr_block = "10.0.0.0/16"
+#   tags = {
+#     terraform = "true"
+#     Name = "testing"
+#   }
+# }
 
-resource "aws_subnet" "publicsubnet" {
-  vpc_id = aws_vpc.myvpc.id
-  cidr_block = "10.0.1.0/24"
-  tags = {
-    terraform = "true"
-    Name = "testing-subnet"
-  }
+# resource "aws_subnet" "publicsubnet" {
+#   vpc_id = aws_vpc.myvpc.id
+#   cidr_block = "10.0.1.0/24"
+#   tags = {
+#     terraform = "true"
+#     Name = "testing-subnet"
+#   }
 
-}
+# }
