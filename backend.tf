2962bb3da53e307f406b8f5c0c4921c035eb9f2f
Devanshu <devanshukumar103@gmail.com>
2025-10-03 17:28:59 +0530

    backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,17 @@
+# terraform {
+#   backend "s3" {
+#     bucket = "deva-terraform-state"
+#     key    = "terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
+
 terraform {
   backend "s3" {
-    bucket = "deva-terraform-state"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
+    bucket                = "terraform-states-deva"
+    dynamodb_table          = "terraform-state-lock-init"
+    encrypt                 = true
+    key                     = "network/state.tfstate"
+    region                  = "eu-central-1"
   }
 }
