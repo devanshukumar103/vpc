9851f98aea4b74dba6549d2050609543c30daafa
Devanshu <devanshukumar103@gmail.com>
2025-10-07 14:02:20 +0530

    vpc

--- network.tf
+++ network.tf
@@ -1,20 +1,20 @@
-# resource "aws_vpc" "myvpc" {
-#   cidr_block = "10.0.0.0/16"
-#   tags = {
-#     terraform = "true"
-#     Name = "vpc1"
-#   }
-# }
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    terraform = "true"
+    Name = "vpc1"
+  }
+}
 
-# resource "aws_subnet" "publicsubnet" {
-#   vpc_id = aws_vpc.myvpc.id
-#   cidr_block = "10.0.1.0/24"
-#   tags = {
-#     terraform = "true"
-#     Name = "subnet1"
-#   }
+resource "aws_subnet" "publicsubnet" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    terraform = "true"
+    Name = "subnet1"
+  }
 
-# }
+}
 
 # module "vpc" {
 #   source  = "terraform-aws-modules/vpc/aws"
