60d9a3a11854ac9422722dd0f7dbb1faab2620d6
Devanshu <devanshukumar103@gmail.com>
2025-10-08 13:46:48 +0530

    cloudtrial

---
+++ cloud.tf
@@ -0,0 +1,45 @@
+resource "aws_cloudtrail" "cloudtrail" {
+  name                          = "cloud"
+  s3_bucket_name                = aws_s3_bucket.cloudtrail_bucket.id
+  s3_key_prefix                 = "dev"
+  include_global_service_events = false
+}
+
+resource "aws_s3_bucket" "cloudtrail_bucket" {
+  bucket        = "terraform-cloudtrail-${data.aws_caller_identity.current.account_id}"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.cloudtrail_bucket.id
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailAclCheck",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "${aws_s3_bucket.cloudtrail_bucket.arn}"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "${aws_s3_bucket.cloudtrail_bucket.arn}/${"dev"}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
