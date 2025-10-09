1baeafddeb0bf5932c0e3bcfd9111486d2e28ad9
Devanshu <devanshukumar103@gmail.com>
2025-10-09 19:11:04 +0530

    vdeletingvpc

--- network.tf
+++ network.tf
@@ -1,10 +1,10 @@
-resource "aws_vpc" "myvpc" {
-  cidr_block = "10.101.0.0/16"
-  tags = {
-    terraform = "true"
-    Name = "vpc02"
-  }
-}
+# resource "aws_vpc" "myvpc" {
+#   cidr_block = "10.101.0.0/16"
+#   tags = {
+#     terraform = "true"
+#     Name = "vpc02"
+#   }
+# }
 
 # # resource "aws_subnet" "publicsubnet" {
 # #   vpc_id = aws_vpc.myvpc.id
@@ -19,71 +19,71 @@ resource "aws_vpc" "myvpc" {
 # ---------------------------
 # Public Subnets (Loop)
 # ---------------------------
-locals {
-  public_subnets = {
-    public-01 = "10.101.12.0/25"
-    public-02 = "10.101.12.128/25"
-    public-03 = "10.101.13.0/25"
-  }
+# locals {
+#   public_subnets = {
+#     public-01 = "10.101.12.0/25"
+#     public-02 = "10.101.12.128/25"
+#     public-03 = "10.101.13.0/25"
+#   }
 
-  private_subnets = {
-    private-01 = "10.101.0.0/22"
-    private-02 = "10.101.4.0/22"
-    private-03 = "10.101.8.0/22"
-  }
+#   private_subnets = {
+#     private-01 = "10.101.0.0/22"
+#     private-02 = "10.101.4.0/22"
+#     private-03 = "10.101.8.0/22"
+#   }
 
-  database_subnets = {
-    database-01 = "10.101.13.128/25"
-    database-02 = "10.101.14.0/25"
-    database-03 = "10.101.14.128/25"
-  }
-}
+#   database_subnets = {
+#     database-01 = "10.101.13.128/25"
+#     database-02 = "10.101.14.0/25"
+#     database-03 = "10.101.14.128/25"
+#   }
+# }
 
-# ---------------------------
-# Public Subnets
-# ---------------------------
-resource "aws_subnet" "public_subnets" {
-  for_each                = local.public_subnets
-  vpc_id                  = aws_vpc.myvpc.id
-  cidr_block              = each.value
-  # map_public_ip_on_launch = true
+# # ---------------------------
+# # Public Subnets
+# # ---------------------------
+# resource "aws_subnet" "public_subnets" {
+#   for_each                = local.public_subnets
+#   vpc_id                  = aws_vpc.myvpc.id
+#   cidr_block              = each.value
+#   # map_public_ip_on_launch = true
 
-  tags = {
-    Name      = each.key
-    Type      = "public"
-    Terraform = "true"
-  }
-}
+#   tags = {
+#     Name      = each.key
+#     Type      = "public"
+#     Terraform = "true"
+#   }
+# }
 
-# ---------------------------
-# Private Subnets
-# ---------------------------
-resource "aws_subnet" "private_subnets" {
-  for_each   = local.private_subnets
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = each.value
+# # ---------------------------
+# # Private Subnets
+# # ---------------------------
+# resource "aws_subnet" "private_subnets" {
+#   for_each   = local.private_subnets
+#   vpc_id     = aws_vpc.myvpc.id
+#   cidr_block = each.value
 
-  tags = {
-    Name      = each.key
-    Type      = "private"
-    Terraform = "true"
-  }
-}
+#   tags = {
+#     Name      = each.key
+#     Type      = "private"
+#     Terraform = "true"
+#   }
+# }
 
-# ---------------------------
-# Database Subnets
-# ---------------------------
-resource "aws_subnet" "database_subnets" {
-  for_each   = local.database_subnets
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = each.value
+# # ---------------------------
+# # Database Subnets
+# # ---------------------------
+# resource "aws_subnet" "database_subnets" {
+#   for_each   = local.database_subnets
+#   vpc_id     = aws_vpc.myvpc.id
+#   cidr_block = each.value
 
-  tags = {
-    Name      = each.key
-    Type      = "database"
-    Terraform = "true"
-  }
-}
+#   tags = {
+#     Name      = each.key
+#     Type      = "database"
+#     Terraform = "true"
+#   }
+# }
 
 
 
