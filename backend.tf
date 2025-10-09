d577e87fd77c3085caa2eeb77c12512bf78136b0
Devanshu <devanshukumar103@gmail.com>
2025-10-09 18:43:05 +0530

    dynamodb

--- backend.tf
+++ backend.tf
@@ -1,19 +1,19 @@
-terraform {
-  backend "s3" {
-    bucket = "deva-tfstate"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
-
 # terraform {
-#    backend "s3" {
-#      bucket         = "deva-terraform-state"   # Make sure this bucket already exists
-#      key            = "network/state.tfstate"
-#      region         = "us-east-1"
-#      encrypt        = true
-#      use_lockfile   = true  # replaces dynamodb_table (deprecated)
-#    }
-#  }
+#   backend "s3" {
+#     bucket = "deva-tfstate"
+#     key    = "terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
+
+
+terraform {
+   backend "s3" {
+     bucket         = "deva-tfstate"   # Make sure this bucket already exists
+     key            = "network/state.tfstate"
+     region         = "us-east-1"
+     encrypt        = true
+     use_lockfile   = true  # replaces dynamodb_table (deprecated)
+   }
+ }
 
