5ac10c32645b3f93fa98df79dbffd2892d2b90d9
Devanshu <devanshukumar103@gmail.com>
2025-10-10 21:58:11 +0530

    db1

--- database.tf
+++ database.tf
@@ -1,38 +1,27 @@
 
-# ##########################
-# # RDS MySQL Instance (Free Tier)
-# ##########################
-# resource "aws_db_instance" "deva_mysql" {
-#   identifier             = "deva-free-db"
-#   engine                 = "mysql"
-#   engine_version         = "8.0"
-#   instance_class         = "db.t3.micro"   # Free Tier eligible
-#   allocated_storage      = 20
-#   storage_type           = "gp2"
-#   username               = "Deva"
-#   password               = "Indian@123"
+##########################
+# RDS MySQL Instance (Free Tier)
+##########################
+resource "aws_db_instance" "deva_mysql" {
+  identifier             = "deva-free-db"
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"   # Free Tier eligible
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  username               = "Deva"
+  password               = "Indian@123"
 
-#   db_subnet_group_name   = aws_db_subnet_group.deva_subnet_group.name
-#   vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.deva_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
 
-#   publicly_accessible    = true
-#   multi_az               = false
-#   skip_final_snapshot    = true
-#   deletion_protection    = false
-#   apply_immediately      = true
+  publicly_accessible    = true
+  multi_az               = false
+  skip_final_snapshot    = true
+  deletion_protection    = false
+  apply_immediately      = true
 
-#   tags = {
-#     Name = "deva-free-tier-db"
-#   }
-# }
-
-# ##########################
-# # Outputs
-# ##########################
-# output "rds_endpoint" {
-#   value = aws_db_instance.deva_mysql.endpoint
-# }
-
-# output "rds_username" {
-#   value = aws_db_instance.deva_mysql.username
-# }
+  tags = {
+    Name = "deva-free-tier-db"
+  }
+}
