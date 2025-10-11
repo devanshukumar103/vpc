3eb8070b05fd273f42c6d26a63ccf38ac6d5e1bd
krishtech1602 <krishkumar1602@gmail.com>
2025-10-11 12:41:32 +0530

    network

--- network.tf
+++ network.tf
@@ -19,6 +19,33 @@ resource "aws_internet_gateway" "igw" {
     Terraform = "true"
   }
 }
+
+# ---------------------------
+# Public Route Table
+# ---------------------------
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name      = "public-rt"
+    Terraform = "true"
+  }
+}
+
+# ---------------------------
+# Associate Public Subnets with Route Table
+# ---------------------------
+resource "aws_route_table_association" "public_associations" {
+  for_each       = aws_subnet.public_subnets
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
 # ---------------------------
 # Public Subnets (Loop)
 # ---------------------------
