5ba63271809344a22e9319c2ce70c6b4397c13a8
krishtech1602 <krishkumar1602@gmail.com>
2025-10-25 22:51:00 +0530

    ec2

--- network/outputs.tf
+++ network/outputs.tf
@@ -1,12 +1,14 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  description = "The ID of the created VPC"
+  value       = aws_vpc.main.id
 }
 
 output "public_subnets" {
-  value = aws_subnet.public[*].id
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
 }
 
 output "private_subnets" {
-  value = aws_subnet.private[*].id
+  description = "List of private subnet IDs"
+  value       = aws_subnet.private[*].id
 }
-#
