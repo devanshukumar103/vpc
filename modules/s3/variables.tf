5c77a97a1e6c1e8972f29bd6d782645d8e937be1
krishtech1602 <krishkumar1602@gmail.com>
2025-10-25 22:10:11 +0530

    bucket

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,6 +1,10 @@
 variable "bucket_name" {
   type        = string
   description = "S3 bucket name"
+  validation {
+    condition     = length(var.bucket_name) <= 60
+    error_message = "Bucket name cannot exceed 60 characters."
+  }
 }
 
 variable "enable_versioning" {
