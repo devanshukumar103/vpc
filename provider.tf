38a1bf47245dce863204994fa28bfeba12164657
krishtech1602 <krishkumar1602@gmail.com>
2025-10-25 20:54:04 +0530

    bucket

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ provider "aws" {
   # access_key = var.access_key
   # secret_key = var.secret_key
 }
-#
+
 # module "my_app_s3_bucket" {
 #   source = "./modules/s3-bucket" # Path to your module directory
 
