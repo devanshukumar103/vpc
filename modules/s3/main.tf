1ac57efad5adb2921a9e83c9a8fa643a793354de
krishtech1602 <krishkumar1602@gmail.com>
2025-10-25 21:30:50 +0530

    bucket

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -19,15 +19,15 @@ resource "aws_s3_bucket_versioning" "this" {
 # ------------------------
 # Example Dynamic Encryption Configuration (optional)
 # ------------------------
-resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
-  bucket = aws_s3_bucket.this.bucket
+# resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+#   bucket = aws_s3_bucket.this.bucket
 
-  dynamic "rule" {
-    for_each = var.enable_versioning ? [1] : []
-    content {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
+#   dynamic "rule" {
+#     for_each = var.enable_versioning ? [1] : []
+#     content {
+#       apply_server_side_encryption_by_default {
+#         sse_algorithm = "AES256"
+#       }
+#     }
+#   }
+# }
