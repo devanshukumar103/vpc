7576b667962fa51bab934bec413064e4651ee9a8
krishtech1602 <krishkumar1602@gmail.com>
2025-10-25 13:43:48 +0530

    bucket

---
+++ modules/s3-bucket/main.tf
@@ -0,0 +1,17 @@
+# modules/s3-bucket/main.tf
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+  acl    = var.acl
+
+  tags = var.tags
+
+  # Enable versioning if specified
+  dynamic "versioning" {
+    for_each = var.enable_versioning ? [1] : []
+    content {
+      enabled = true
+    }
+  }
+
+  # Add other S3 bucket configurations as needed (e.g., server-side encryption, lifecycle rules)
+}
--- provider.tf
+++ provider.tf
@@ -3,3 +3,15 @@ provider "aws" {
   # access_key = var.access_key
   # secret_key = var.secret_key
 }
+
+module "my_app_s3_bucket" {
+  source = "./modules/s3-bucket" # Path to your module directory
+
+  bucket_name       = "my-unique-app-bucket-name-ashok"
+  acl               = "private"
+  enable_versioning = true
+  tags = {
+    Environment = "Development"
+    Project     = "MyApp"
+  }
+}
