02eac7fc03f69d334f9a8ea551a69405adb06486
krishtech1602 <krishkumar1602@gmail.com>
2025-10-25 21:13:17 +0530

    bucket

---
+++ modules/s3-bucket.org/outputs.tf
@@ -0,0 +1,11 @@
+# # modules/s3-bucket/outputs.tf
+# output "bucket_id" {
+#   description = "The ID (name) of the S3 bucket."
+#   value       = aws_s3_bucket.this.id
+# }
+
+# output "bucket_arn" {
+#   description = "The ARN of the S3 bucket."
+#   value       = aws_s3_bucket.this.arn
+# }
+
