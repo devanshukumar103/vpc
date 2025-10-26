2252d000ebe5f2cd57e0786705aaacb906b65865
krishtech1602 <krishkumar1602@gmail.com>
2025-10-26 19:47:41 +0530

    bucket

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "s3_bucket" {
   source = "./modules/s3-bucket"
 
-  bucket_name       = "myapp-dev-bucket-ashokkumar"
+  #bucket_name       = "myapp-dev-bucket-ashokkumar"
   #enable_versioning = true
 
-  custom_tags = {
-    Owner       = "Ashok Kumar"
-    Environment = "dev"
-  }
+#   custom_tags = {
+#     Owner       = "Ashok Kumar"
+#     Environment = "dev"
+#   }
 }
