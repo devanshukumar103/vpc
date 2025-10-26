17668c3464f03813bf367f3f4fd90843575272e5
krishtech1602 <krishkumar1602@gmail.com>
2025-10-26 13:47:01 +0530

    vpc

--- ec2.tf
+++ ec2.tf
@@ -19,4 +19,4 @@ resource "aws_instance" "app" {
     Name = "example-instance"
   }
 }
-###
+####
---
+++ networks.tf
@@ -0,0 +1,30 @@
+# --------------------------------------------------------
+# Create a VPC
+# --------------------------------------------------------
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "myvpc3"
+  }
+}
+
+# --------------------------------------------------------
+# Create a Public Subnet
+# --------------------------------------------------------
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
