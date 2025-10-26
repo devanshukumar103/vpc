34ca6b60bf1fe93475d3623ee38ba8077ecc708f
krishtech1602 <krishkumar1602@gmail.com>
2025-10-26 13:50:21 +0530

    vpc

--- network/outputs.tf
+++ network/outputs.tf
@@ -5,9 +5,13 @@ output "vpc_id" {
   value       = aws_vpc.myvpc.id
 }
 
-output "public_subnet" {
-  description = "List of public subnet IDs"
-  # This handles either count or for_each
-  value       = [for s in aws_subnet.public_subnets : s.id]
-}
+# output "public_subnet" {
+#   description = "List of public subnet IDs"
+#   # This handles either count or for_each
+#   value       = [for s in aws_subnet.public_subnets : s.id]
+# }
 #
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
